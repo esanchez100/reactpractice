896f5945c07b8a3ae00aa5f56cdb30d6055311dd
Emily Sanchez <emily.sanchez@contractor.farmersinsurance.com>
2019-08-08 12:36:45 -0700

    small setup changes

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+
+
+class Main extends Component{
+    state= {
+
+    };
+
+    render(){
+        return (
+        <div>
+        <h1>Hello!</h1>
+ 
+        </div>)
+    }
+}
+
+export default Main;
